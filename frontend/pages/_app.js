1ef4fc99b7a1242e3bc2783fc182801b7d5d0035
Zino Kader <zino@zinokader.se>
2019-07-13 23:23:23 +0200

    removed unnecessary getInitialProps, allowing automatic prerendering

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -26,14 +26,4 @@ const MyApp = ({ Component, pageProps }) => {
   );
 };
 
-MyApp.getInitialProps = async ({ Component, ctx }) => {
-  let pageProps = {};
-
-  if (Component.getInitialProps) {
-    pageProps = await Component.getInitialProps(ctx);
-  }
-
-  return { pageProps };
-};
-
 export default MyApp;
