7bca5720ec88777b33d99a69e11014a236f372ef
Zino Kader <zino@zinokader.se>
2019-07-13 22:34:36 +0200

    added constants

--- frontend/global/global.js
+++ frontend/global/global.js
@@ -1,1 +1,8 @@
 export const kth_mail_domain = "@kth.se";
+
+export const kth_profile_edit_url = email => {
+  const account_name = email.replace(kth_mail_domain, "");
+  return "https://www.kth.se/profile/" + account_name + "/edit/";
+};
+export const profile_picture_placeholder_path =
+  "../../static/profile_placeholder.png";
