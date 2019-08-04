956c341d3c7784b54b4be6216c1b4b3881cc1c74
Zino Kader <zino@zinokader.se>
2019-08-04 23:16:23 +0200

    add SEO meta tags

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -19,8 +19,18 @@ export default class extends App {
       <Container>
         <Component {...pageProps}>
           <Head>
-            <meta name="description" content="ÄNDRA SEN!!!" />
-            <meta key="themeColor" name="theme-color" content="#ffffff" />
+            <meta name="description" content="KTHKIT - Den kompletta verktygslådan för KTH-studenten.
+             Räkna snittbetyg, ladda ned gamla tentor, se tentastatistik och mer." />
+            <link rel="apple-touch-icon" sizes="180x180" href="/static/favicon/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon/favicon-16x16.png" />
+            <link rel="manifest" href="/static/favicon/site.webmanifest" />
+            <link rel="mask-icon" href="/static/favicon/safari-pinned-tab.svg" color="#fafafa" />
+            <link rel="shortcut icon" href="/static/favicon/favicon.ico" />
+            <meta name="msapplication-TileColor" content="#fafafa" />
+            <meta name="msapplication-config" content="/static/favicon/browserconfig.xml" />
+            <meta key="themeColor" name="theme-color" content="#fafafa" />
+
             <meta key="IE" httpEquiv="X-UA-Compatible" content="IE=edge" />
             <meta
               name="viewport"
