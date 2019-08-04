a72893a0eb3335ac0ddbf2bed7a00d2fec61064e
Zino Kader <zino@zinokader.se>
2019-08-04 23:44:08 +0200

    move SEO tags to _document

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -17,27 +17,7 @@ export default class extends App {
 
     return (
       <Container>
-        <Component {...pageProps}>
-          <Head>
-            <meta name="description" content="KTHKIT - Den kompletta verktygslådan för KTH-studenten.
-             Räkna snittbetyg, ladda ned gamla tentor, se tentastatistik och mer." />
-            <link rel="apple-touch-icon" sizes="180x180" href="/static/favicon/apple-touch-icon.png" />
-            <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon/favicon-32x32.png" />
-            <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon/favicon-16x16.png" />
-            <link rel="manifest" href="/static/favicon/site.webmanifest" />
-            <link rel="mask-icon" href="/static/favicon/safari-pinned-tab.svg" color="#fafafa" />
-            <link rel="shortcut icon" href="/static/favicon/favicon.ico" />
-            <meta name="msapplication-TileColor" content="#fafafa" />
-            <meta name="msapplication-config" content="/static/favicon/browserconfig.xml" />
-            <meta key="themeColor" name="theme-color" content="#fafafa" />
-
-            <meta key="IE" httpEquiv="X-UA-Compatible" content="IE=edge" />
-            <meta
-              name="viewport"
-              content="width=device-width, initial-scale=1"
-            />
-          </Head>
-        </Component>
+        <Component {...pageProps} />
       </Container>
     );
   }
---
+++ frontend/pages/_document.js
@@ -0,0 +1,59 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta
+            name="description"
+            content="KTHKIT - Den kompletta verktygslådan för KTH-studenten.
+             Räkna snittbetyg, ladda ned gamla tentor, se tentastatistik och mer."
+          />
+          <meta name="msapplication-TileColor" content="#fafafa" />
+          <meta
+            name="msapplication-config"
+            content="/static/favicon/browserconfig.xml"
+          />
+          <meta key="themeColor" name="theme-color" content="#fafafa" />
+
+          <meta key="IE" httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/static/favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/static/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/static/favicon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/static/favicon/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/static/favicon/safari-pinned-tab.svg"
+            color="#fafafa"
+          />
+          <link rel="shortcut icon" href="/static/favicon/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
