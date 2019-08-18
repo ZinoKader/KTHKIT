142cd2dbef0f3b079070c43f7bb76a91127d7cbe
Zino Kader <zino@zinokader.se>
2019-08-18 14:49:18 +0200

    course information link added

--- frontend/components/Course/Statistic/index.js
+++ frontend/components/Course/Statistic/index.js
@@ -1,15 +1,23 @@
 import React from "react";
 import "./styles.scss";
+import Link from "next/link";
 
 const Statistic = ({ courseItem }) => {
   return (
-    <div>
+    <>
+      <a
+        className="courseInformation"
+        href={courseItem.courseLink}
+        target="_blank"
+      >
+        Kursinformation
+      </a>
       <div className="courseDescriptionContainer">
         {courseItem.courseDescription.length !== 0
           ? courseItem.courseDescription
           : "Kursen saknar beskrivning"}
       </div>
-    </div>
+    </>
   );
 };
 
